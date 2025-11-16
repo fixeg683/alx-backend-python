28cbc71f72faaa1231c5febf0046794e6a4ff6c2
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-11-16 19:15:22 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,13 +1,30 @@
-from rest_framework import viewsets
-from .models import Message, Conversation
-from .serializers import MessageSerializer, ConversationSerializer
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['conversation__conversation_id', 'sender__username']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
