019a7ac9435614577e136e7515c3d000831199a4
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-11-16 19:26:42 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path("", lambda request: redirect("api/")),   # root → /api/
+    path("", lambda request: redirect("api/")),
     path("admin/", admin.site.urls),
-    path("api/", include("chats.urls")),
+    path("api/", include("chats.urls")),        # app routes
+    path("api-auth/", include("rest_framework.urls")),  # ✅ add api-auth
 ]
